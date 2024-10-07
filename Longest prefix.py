73aeb7d72c58c8328155d21b28a50b9392ec7031
Prashanth <151809864+Prashanth-000@users.noreply.github.com>
2024-10-07 19:03:45 +0530

    Update and rename Longest prefix to Longest prefix.py

--- Longest prefix.py
+++ Longest prefix.py
@@ -1,4 +1,4 @@
-/* Write a function to find the longest common prefix string amongst an array of strings.*/
+// Write a function to find the longest common prefix string amongst an array of strings.
 class Solution(object):
     def longestCommonPrefix(self, strs):
         min=200
