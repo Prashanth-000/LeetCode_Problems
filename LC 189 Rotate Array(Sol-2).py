420b524fb87fa178fb799d83f0c01e26e52492e4
Prashanth <151809864+Prashanth-000@users.noreply.github.com>
2024-11-28 23:07:29 +0530

    Create LC 189 Rotate Array(Sol-2).py

---
+++ LC 189 Rotate Array(Sol-2).py
@@ -0,0 +1,19 @@
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+class Solution(object):
+    def rotate(self, nums, k):
+        l=len(nums)
+        ans=nums[:]
+        for i in range(0,l):
+            nums[(i+k)%l]=ans[i]
+# Runtime
+# 7
+# ms
+# Beats
+# 50.87%
+# Analyze Complexity
+# Memory
+# 25.58
+# MB
+# Beats
+# 6.58%
+# Analyze Complexity
