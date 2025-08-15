7aa610f72b03851f355f876048cd58cbd40b0b43
prashanth <151809864+Prashanth-000@users.noreply.github.com.>
2025-08-15 17:05:59 +0530

    Buffer reader for java

---
+++ main_one.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class main_one {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println("Number entered: " + n);
+    }
+}
